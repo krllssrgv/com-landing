25515ff1146cd8888bbe2491f959a41e6286cf28
Kirill Savin <krllssrgv@gmail.com>
2025-02-19 20:21:58 +0300

    Сделал структуру сайта

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './app.css';
+import './styles/header.scss';
+import './styles/intro.scss';
+import './styles/about.scss';
+import './styles/certificates.scss';
+import './styles/patterns.scss';
+import './styles/edu.scss';
+import './styles/clients.scss';
+import './styles/reviews.scss';
+import './styles/request.scss';
+import './styles/footer.scss';
+
