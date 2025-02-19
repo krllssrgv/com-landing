4e461a57ddac9cb626173155641553615c19f2ba
Kirill Savin <krllssrgv@gmail.com>
2025-02-19 12:47:07 +0300

    Структура страницы + инструменты создания

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "semi": ["error", "always"],
+    },
+  },
+];
